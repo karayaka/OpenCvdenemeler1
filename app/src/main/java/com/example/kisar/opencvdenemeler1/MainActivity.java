73dec2cce46f8d14faf9b666f05cf35be0dd0345
çağrı KARAYAKA <cagri.karayaka@gmail.com>
2019-06-11 18:29:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kisar/opencvdenemeler1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.kisar.opencvdenemeler1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Mat mat=new Mat();
+        mat.toString();
+    }
+}
